f15ee89d5f8bb9e0f9cdbc91475bafc43fc85cda
chrisTEvans0808 <ctevans0042@gmail.com>
2023-05-02 16:19:38 -0500

    Fixed account-editing.dart page. Added fixed images and new images.

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: StartScreen(),
+      home: MenuScreen(),
 
     );
   }
